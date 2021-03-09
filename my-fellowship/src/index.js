a4f0003da01759d09372b09fba64ffc65685b63a
APiligrim <anastasiyauraleva@gmail.com>
2021-03-09 15:28:33 -0500

    Add: create react app with github auth

---
+++ my-fellowship/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import GithubAuth from "./GithubAuth";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <GithubAuth />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
