ae42727247c24f8e9fea3fc586927cf153883ce1
Sarthak Khattar <sarthakoct@gmail.com>
2021-03-14 06:41:15 +0530

    idk why this file is here

--- my-fellowship/src/data/GraphQL.js
+++ my-fellowship/src/data/GraphQL.js
@@ -1,10 +1,7 @@
 import { gql } from "apollo-boost";
 
-<<<<<<< HEAD
 //here we are doing apollo boost - this query will have these params 
 // if we have more params, we put them in query , and use it in where ever u need it 
-=======
->>>>>>> d6681f4 (added live data endpoints and contected graphQL)
 export const GET_ALL_USER_INFO = gql`
   query($username: String!) {
     user(login: $username) {
