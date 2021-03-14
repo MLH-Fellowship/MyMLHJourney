d6df92e036347dbb4e502e0f596a996769edaddb
Anastasiya Uraleva <anastasiyauraleva@gmail.com>
2021-03-13 20:40:55 -0500

    Added: Rest of landing page boogiewoogie oopsie(#22)
    * Add: create react app with github auth
    
    * Update: Github Authentication with Firebase is set up
    
    * added live data endpoints and contected graphQL
    
    * Added: 6cards for the visualizations
    
    * Add: Skeleton of the landing page
    
    * removed package-log
    
    * Added: Rest of the Landing Page
    
    Co-authored-by: Sarthak Khattar <sarthakoct@gmail.com>

--- my-fellowship/src/cards/LandingPage.js
+++ my-fellowship/src/cards/LandingPage.js
@@ -4,7 +4,11 @@ import Navbar from "../components/navbar";
 import globe from "./images/globe.png";
 import Conclusion from "./images/last-section.gif";
 import MovingCircles from "./images/moving-circles2.gif";
+import Footer from "../components/footer.js";
 import sun from "./images/sun.png";
+import hills from "./images/hills.png";
+import card from "./images/card.png";
+
 class LandingPage extends Component {
   render() {
     return (
@@ -17,32 +21,62 @@ class LandingPage extends Component {
               <h1 id="header-title">
                 Welcome to <br /> Your MLH Journey
               </h1>
-              <button>TRACK MY PROJECT</button>
+              <button>TRACK MY JOURNEY</button>
             </div>
             <div>
               <img id="globe" src={globe} />
             </div>
           </section>
+          <img className="hills" src={hills} />
 
-          {/* <section className="center-text">
-            <h1 id="header-title">
-              Get a Glimpse of <br /> Your Progress
-            </h1>
-          </section>
+          <br />
+          <div id="wrapper" className="sun-section">
+            <div id="sticky" className="sun-section">
+              <img className="sticky" src={sun} />
+            </div>
+            <section className="center-text top-overlay">
+              <h1 id="header-title">
+                Get a Glimpse of <br /> Your Progress
+              </h1>
+              <p>
+                View your achievemements and insights <br /> through handmade
+                cards
+              </p>
+            </section>
 
-          <div className="sun-cont container">
-            <div className="sun-offset sun"></div>
+            <section className="two-columns ">
+              <div className="center-image">
+                <img src={card} />
+              </div>
+              <div className="center-image">
+                <img src={card} />
+              </div>
+            </section>
 
+            <section className="two-columns">
+              <div className="center-image">
+                <img src={card} />
+              </div>
+              <div className="center-image">
+                <img src={card} />
+              </div>
+            </section>
           </div>
-          <img src={sun} /> */}
-          <section>
+          <br />
+
+          <section className="center-text">
             <img src={MovingCircles} />
+            <p>It is time to discover your journey</p>
+          </section>
+          <section className="last-section">
+            <img src={Conclusion} />
+
+            <div className="center-button">
+              <button className="start-my-journey">Start My Journey</button>
+            </div>
           </section>
 
-          <section>
-            <button>Start My Journey</button>
-            <img src={Conclusion} />
-          </section>
+          <Footer />
         </body>
       </div>
     );
