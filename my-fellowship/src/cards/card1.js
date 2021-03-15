9669b39406632d612e86a5a1f97f417d2729d487
APiligrim <anastasiyauraleva@gmail.com>
2021-03-15 01:02:11 -0400

    Added: First card is done

--- my-fellowship/src/cards/card1.js
+++ my-fellowship/src/cards/card1.js
@@ -3,6 +3,8 @@ import { useLazyQuery, useQuery } from "@apollo/react-hooks";
 import { useUser } from "../context/userContext";
 import "./styles/card1.css";
 import FirstCardBackground from "./images/first-card-background.gif";
+import githubIcon from "./images/github.png";
+import webIcon from "./images/web.png";
 
 const FirstCard = (props) => {
   // let obj = props.contributionData;
@@ -17,8 +19,20 @@ const FirstCard = (props) => {
         <section className="first-card-data">
           <img id="avatar" src={obj.avatarUrl} />
           <p id="name">{obj.name}</p>
-          <p id="location">Based in {obj.location}</p>
-          <p id="location"></p>
+          <p className="location">Based in {obj.location}</p>
+
+          <ul className="social-media-list">
+            <li>
+              <a href={obj.url}>
+                <img src={githubIcon} alt="GitHub" title="GitHub" />
+              </a>
+            </li>
+            <li>
+              <a href={obj.websiteUrl}>
+                <img src={webIcon} alt="Portfolio Website" title="Website" />
+              </a>
+            </li>
+          </ul>
         </section>
         <img className="first-card-background" src={FirstCardBackground} />
       </body>
