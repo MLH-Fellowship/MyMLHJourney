7bc6b8c88fbc75ca6c04371ebda9bfaf24a5d266
APiligrim <anastasiyauraleva@gmail.com>
2021-03-15 05:26:42 -0400

    Added: Card 2 styling basics

--- my-fellowship/src/cards/card2.js
+++ my-fellowship/src/cards/card2.js
@@ -1,6 +1,8 @@
 import react from "react";
 import { useLazyQuery, useQuery } from "@apollo/react-hooks";
 import { useUser } from "../context/userContext";
+import SecondCardBackground from "./images/second-card-background.gif";
+import "./styles/card2.css";
 
 const SecondCard = (props) => {
   let obj = props.contributionData;
@@ -35,12 +37,33 @@ const SecondCard = (props) => {
 
   return (
     <div>
-      <h1>This is 2nd card</h1>
-      <p>
-        {/* {obj.name} by {obj.location} */}
-      </p>
+      <body className="ThirdCard">
+        <section className="third-card-data">
+          <div id="flexbox">
+            <div>
+              <h1 className="counter-numbers">dfsd</h1>
+              <p className="title">Standups</p>
+            </div>
+            <div>
+              <h1 className="counter-numbers">fdsdf</h1>
+              <p className="title">Meeting Hours</p>
+            </div>
+          </div>
+          <div id="flexbox" className="flexbox-gap">
+            <div>
+              <h1 className="counter-numbers">dfsd</h1>
+              <p className="title">Standups</p>
+            </div>
+            <div>
+              <h1 className="counter-numbers">fdsdf</h1>
+              <p className="title">Meeting Hours</p>
+            </div>
+          </div>
+        </section>
+        <img clasName="third-card-background" src={SecondCardBackground} />
+      </body>
     </div>
   );
-}
+};
 
 export default SecondCard;
