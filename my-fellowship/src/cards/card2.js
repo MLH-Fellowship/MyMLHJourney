40e2e7ce5df5899ffe3ead58e3d4515b1adc45c0
Sarthak Khattar <sarthakoct@gmail.com>
2021-03-15 14:54:19 +0530

    cleaned contribution data (#29)
    * fixed data query
    
    * cleaned contribution data

--- my-fellowship/src/cards/card2.js
+++ my-fellowship/src/cards/card2.js
@@ -4,8 +4,34 @@ import { useUser } from "../context/userContext";
 
 const SecondCard = (props) => {
   let obj = props.contributionData;
+  // Number of PRs the user has in the MLH-Organization repos
+  let numOfPRs = obj.search.edges.length;
+  // Total lines added
+  let linesAdded = 0;
+  // Total lines deleted
+  let linesDeleted = 0;
+  // Approx amount of coffee cups ~ number of PRs
+  let coffeeCups = numOfPRs;
+  // Links to all the PRs
+  let prLinks = [];
+  // Dates of creation of PRs
+  let prDates = [];
+  // Repositories contributed to
+  let repoList = [];
 
-  console.log(props);
+  for (var i = 0; i < obj.search.edges.length; i++) {
+    let prObj = obj.search.edges[i].node;
+
+    prLinks.push(prObj.url);
+    prDates.push(prObj.createdAt);
+
+    if (!repoList.includes(prObj.headRepository.name)) {
+      repoList.push(prObj.headRepository.name);
+    };
+
+    linesAdded += prObj.additions;
+    linesDeleted += prObj.deletions;
+  }
 
   return (
     <div>
