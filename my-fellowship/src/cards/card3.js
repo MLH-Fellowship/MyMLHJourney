38279e03f1d28ccf30ed4418aa2c9c3940fb402f
Sarthak Khattar <sarthakoct@gmail.com>
2021-03-15 11:40:15 +0530

    fixed data query

--- my-fellowship/src/cards/card3.js
+++ my-fellowship/src/cards/card3.js
@@ -1,6 +1,5 @@
 import react from "react";
 import { useLazyQuery, useQuery } from "@apollo/react-hooks";
-import { GET_ALL_USER_INFO } from "../data/GraphQL";
 import { useUser } from "../context/userContext";
 import ThirdCardBackground from "./images/third-card-background.gif";
 import "./styles/card3.css";
